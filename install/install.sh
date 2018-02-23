7407968d34ae29c8cd435a866083be7c608ea6e6
jerome <jerome@idsia.ch>
2018-02-23 16:30:59 +0100

    Split setup script

--- install/install.sh
+++ install/install.sh
@@ -41,14 +41,6 @@ pip3 install docker-compose
 
 printf "\n****** Docker installation done ******\n"
 
-# Clone repo
-
-printf "\n****** Cloning the repository ****** \n"
-
-mkdir -p /root/docker
-git clone https://github.com/jeguzzi/mighty-thymio.git /root/docker/mighty-thymio
-
-cd /root/docker/mighty-thymio/install
 
 #https://wiki.odroid.com/accessory/connectivity/wifi/wlan_ap
 
@@ -62,31 +54,16 @@ cp ./hostapd /usr/sbin/hostapd
 
 ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 
+mkfifo /var/run/wlan
+
+# Clone repo
+
+printf "\n****** Cloning the repository ****** \n"
+
+mkdir -p /root/docker
+git clone https://github.com/jeguzzi/mighty-thymio.git /root/docker/mighty-thymio
+
+
 printf "\n****** Installation done, now run the config script ******\n"
 
-read -e -p "Enter the robot name: " -i "thymio" name
-read -e -p "Enter the wifi password: " password
-read -e -p "Enter the wifi channel: " -i "6" channel
-
-aps=""; while true
-do
-  read -e -p "Enter the access point ssid: " -i "" ssid
-  if [ "$ssid" == "" ]
-  then
-    break
-  fi
-  read -e -p "Enter the access point password: " ap_password
-  if [ "$aps" != "" ]
-  then
-    aps+=" "
-  fi
-  aps+=$ssid" "$ap_password
-done; echo $aps
-
-python3 setup.py --name $name --password $password --channel $channel --access_points $aps
-
-printf "\n****** Configuration done ******\n"
-
-cd /root/docker/mighty-thymio
-
-mkfifo /var/run/wlan
+cd /root/docker/mighty-thymio/install
---
+++ install/setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+read -e -p "Enter the robot name: " -i "thymio" name
+read -e -p "Enter the wifi password: " password
+read -e -p "Enter the wifi channel: " -i "6" channel
+
+aps=""; while true
+do
+  read -e -p "Enter the access point ssid: " -i "" ssid
+  if [ "$ssid" == "" ]
+  then
+    break
+  fi
+  read -e -p "Enter the access point password: " ap_password
+  if [ "$aps" != "" ]
+  then
+    aps+=" "
+  fi
+  aps+=$ssid" "$ap_password
+done; echo $aps
+
+python3 setup.py --name $name --password $password --channel $channel --access_points $aps
+
+printf "\n****** Configuration done ******\n"
