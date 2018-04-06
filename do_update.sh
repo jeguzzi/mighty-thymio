c406c9f1fff8fea24f152ed4b5fd21fc29d39b57
jerome <jerome@idsia.ch>
2018-04-06 12:02:35 +0200

    Inverted

--- do_update.sh
+++ do_update.sh
@@ -4,7 +4,7 @@ cd /root/docker/mighty-thymio
 
 git pull
 docker-compose down
-docker system prune -f
 docker-compose pull
+docker system prune -f
 source services.env
 docker-compose up -d $SERVICES
