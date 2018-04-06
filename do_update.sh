5059c60e6fcba9b4e4e52ab6275f229396728f1c
jerome <jerome@idsia.ch>
2018-04-06 12:05:13 +0200

    Selected pulling

--- do_update.sh
+++ do_update.sh
@@ -4,7 +4,7 @@ cd /root/docker/mighty-thymio
 
 git pull
 docker-compose down
-docker-compose pull
+docker-compose pull robot portainer
 docker system prune -f
 source services.env
 docker-compose up -d $SERVICES
