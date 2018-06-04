4d73041dc3fe2cc53f8d2c8154b47abe5227657d
jerome <jerome@idsia.ch>
2018-06-04 11:55:38 +0200

    Typo

--- check_docker.sh
+++ check_docker.sh
@@ -17,26 +17,27 @@ if ( ! systemctl -q is-active docker)
   systemctl start docker
 fi
 
-#
-# if [[ ! $(systemctl is-active --quiet docker) ]]; then
-#   systemctl stop docker
-#   rm -r /var/lib/docker/runtimes
-#   rm -r /var/lib/docker/containers
-#   sleep 2
-#   logger -s "Remove /var/lib/docker/runtimes, /var/lib/docker/containers and restart the docker service"
-#   systemctl start docker
-# fi
-#
-# if [[ ! $(systemctl is-active --quiet docker) ]]; then
-#   logger -s "Could not launch docker"
-#   return 1
-# fi
-#
-# # Check that the (essential) services are up
-#
-# cd /root/docker/mighty-thymio
-# services=$(docker-compose ps | wc -l)
-# if [[ "$services" -eq "2" ]]; then
-#   logger -s "Recreate services"
-#   source do_update.sh
-# fi
+if ( ! systemctl -q is-active docker)
+  then
+  logger -s "Remove /var/lib/docker/runtimes, /var/lib/docker/containers and restart the docker service"
+  systemctl stop docker
+  rm -r /var/lib/docker/runtimes
+  rm -r /var/lib/docker/containers
+  systemctl start docker
+fi
+
+if ( ! systemctl -q is-active docker)
+  then
+  logger -s "Could not launch docker"
+  return 1
+fi
+
+# Check that the (essential) services are up
+
+cd /root/docker/mighty-thymio
+services=$(docker-compose ps | wc -l)
+if [ $services == "2" ]
+  then
+  logger -s "Recreate services"
+  source do_update.sh
+fi
