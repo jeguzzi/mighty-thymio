9c9de1db15503b302e5a8c2d1b293052519c084a
jerome <jerome@idsia.ch>
2018-06-04 11:12:54 +0200

    Added docker (boot) sanity check

---
+++ check_docker.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Check that docker is up
+
+if ! [[ systemctl is-active --quiet docker ]]; then
+  systemctl stop docker
+  logger -s "Restart the docker service"
+  systemctl start docker
+fi
+
+if ! [[ systemctl is-active --quiet docker ]]; then
+  systemctl stop docker
+  rm -r /var/lib/docker/runtimes
+  logger -s "Remove /var/lib/docker/runtimes and restart the docker service"
+  systemctl start docker
+fi
+
+if ! [[ systemctl is-active --quiet docker ]]; then
+  systemctl stop docker
+  rm -r /var/lib/docker/runtimes
+  rm -r /var/lib/docker/containers
+  logger -s "Remove /var/lib/docker/runtimes, /var/lib/docker/containers and restart the docker service"
+  systemctl start docker
+fi
+
+if ! [[ systemctl is-active --quiet docker ]]; then
+  logger -s "Could not launch docker"
+  exit 1
+fi
+
+
+# Check that the (essential) services are up
+
+cd /root/docker/mighty-thymio
+services=$(docker-compose ps | wc -l)
+if [[ "$services" -eq "2" ]]; then
+  logger -s "Recreate services"
+  source do_update.sh
+fi
