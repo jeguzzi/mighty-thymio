c7850049e8bec6b9a5a7e7600dd4a3381a8860dc
jerome <jerome@idsia.ch>
2018-06-04 11:34:39 +0200

    Typo

--- check_docker.sh
+++ check_docker.sh
@@ -7,33 +7,33 @@ if [[ ! $(systemctl is-active --quiet docker) ]]; then
   systemctl stop docker && systemctl start docker
 fi
 
-if [[ ! $(systemctl is-active --quiet docker) ]]; then
-  systemctl stop docker
-  rm -r /var/lib/docker/runtimes
-  sleep 2
-  logger -s "Remove /var/lib/docker/runtimes and restart the docker service"
-  systemctl start docker
-fi
-
-if [[ ! $(systemctl is-active --quiet docker) ]]; then
-  systemctl stop docker
-  rm -r /var/lib/docker/runtimes
-  rm -r /var/lib/docker/containers
-  sleep 2
-  logger -s "Remove /var/lib/docker/runtimes, /var/lib/docker/containers and restart the docker service"
-  systemctl start docker
-fi
-
-if [[ ! $(systemctl is-active --quiet docker) ]]; then
-  logger -s "Could not launch docker"
-  return 1
-fi
-
-# Check that the (essential) services are up
-
-cd /root/docker/mighty-thymio
-services=$(docker-compose ps | wc -l)
-if [[ "$services" -eq "2" ]]; then
-  logger -s "Recreate services"
-  source do_update.sh
-fi
+# if [[ ! $(systemctl is-active --quiet docker) ]]; then
+#   systemctl stop docker
+#   rm -r /var/lib/docker/runtimes
+#   sleep 2
+#   logger -s "Remove /var/lib/docker/runtimes and restart the docker service"
+#   systemctl start docker
+# fi
+#
+# if [[ ! $(systemctl is-active --quiet docker) ]]; then
+#   systemctl stop docker
+#   rm -r /var/lib/docker/runtimes
+#   rm -r /var/lib/docker/containers
+#   sleep 2
+#   logger -s "Remove /var/lib/docker/runtimes, /var/lib/docker/containers and restart the docker service"
+#   systemctl start docker
+# fi
+#
+# if [[ ! $(systemctl is-active --quiet docker) ]]; then
+#   logger -s "Could not launch docker"
+#   return 1
+# fi
+#
+# # Check that the (essential) services are up
+#
+# cd /root/docker/mighty-thymio
+# services=$(docker-compose ps | wc -l)
+# if [[ "$services" -eq "2" ]]; then
+#   logger -s "Recreate services"
+#   source do_update.sh
+# fi
