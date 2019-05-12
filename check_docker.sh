9516041627026143e38f1778b96aa4b38b9e2b89
Jérôme Guzzi <jerome@idsia.ch>
2019-05-12 13:04:24 +0200

    Update check_docker.sh

--- check_docker.sh
+++ check_docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Delete garbage folder
+rm -rf /var/lib/docker/containers_
+
 # Check that docker is up
 
 if ( ! systemctl -q is-active docker)
@@ -13,7 +16,7 @@ if ( ! systemctl -q is-active docker)
   then
   logger -s "Remove /var/lib/docker/runtimes and restart the docker service"
   systemctl stop docker
-  rm -r /var/lib/docker/runtimes
+  rm -rf /var/lib/docker/runtimes
   systemctl start docker
 fi
 
@@ -21,9 +24,10 @@ if ( ! systemctl -q is-active docker)
   then
   logger -s "Remove /var/lib/docker/runtimes, /var/lib/docker/containers and restart the docker service"
   systemctl stop docker
-  rm -r /var/lib/docker/runtimes
-  rm -r /var/lib/docker/containers
-  rm -r /var/lib/docker/contained
+  rm -rf /var/lib/docker/runtimes
+  rm -rf /var/lib/docker/containers
+  rm -rf /var/lib/docker/containerd
+  mv /var/lib/docker/containers /var/lib/docker/containers_
   systemctl start docker
 fi
 
